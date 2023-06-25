2e39dde1e685f458fb9686ebf7493be12b23cd07
willr4in <113215579+willr4in@users.noreply.github.com>
2023-06-25 13:58:20 +0300

    Update Node.cpp

--- Shambilov/KP8/src/Node.cpp
+++ Shambilov/KP8/src/Node.cpp
@@ -5,3 +5,13 @@ Node<T>::Node(const T& value, Node<T>* next) {
     this->nextNode = next;
     this->data = value;
 }
+
+template<typename T>
+T& Node<T>::get_data() const {
+    return data;
+}
+
+template<typename T>
+Node<T>* Node<T>::get_nextNode() const {
+    return nextNode;
+}
